a394c9d7a0c34b0622f20b87c73c066e80f43a5f
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-10-21 21:10:41 +0800

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,19 @@
+#lanjutan dasar-dasar phyton
+#Aturan 6 :sebagai penanda string, phyton menggunakan tanda kutip, baik tunggal ('). maupun ganda(").
+#tanda kutip tiga (""") di gunakan untuk string multibaris.
+#contoh:
+nama = "andini"
+namapanjang = """Suryandini"""
+print (namapanjang)
+
+#Aturan 7 : Bahasa phyton bersifat case sensitif. yang berarti membedakan huruf besar dan
+#huruf kecil (memiliki nilai yang berbeda)
+#contoh:
+luas = 2000
+print (luas)
+
+#Aturab 8: untuk komentar di awali dengan tanda '#' yang artinya tidak diproses sebagai statement code
+#contoh:
+#Perintah untuk menghitung luas
+luastanah = 3000
+print("luastanah", luastanah)

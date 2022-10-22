a60677ee9b943623d1cb1c89b486bc2290557fb7
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-10-22 18:47:22 +0800

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,34 @@
+#Variabel pada phyton
+#variabel adalah likasi memory yang dapat di gunakan untuk menyimpan sebuah nilai dimana nilai tersebut dapat
+#di ubah sewaktu-waktu sesuai dengan kebutuhan pemakainya.variabel di dalam phyton mempunyai sifat yang dinamis dan tidak perlu di deklarasikan terlebih dahulu
+#dengan tipe data tertentu.variabel phyton juga dapat di ubah saat program di jalankan.
+#untuk deklarasi variabel di phyton cukup menuliskan nama variabel, kemudian mengisinya dengan suatu nilai.tambahkan tanda "sama dengan" atau "=" antara nama variabel dan nilai yang ingin di masukan.
+#contoh:
+nama = "suryandini"
+print ("nama =", nama)
+
+#Aturan 1 : karakter pertama harus berupa huruf atau garis bawah. karakter berikutnya yang dapat digunakan adalah huruf,angka atau garis bawah.
+#contoh:
+nama_siswi1 = "Alya"
+nama_siswi = "Tika"
+nama_siswi3 = "Dini"
+print ("nama_siswi1",nama_siswi1)
+print ("nama_siswi",nama_siswi)
+print ("nama_siswi3",nama_siswi3)
+
+#Aturan 2 : panjang variabel tidak terbatas oleh jumblah karakter tetatpi sebaiknya di gunakan seperlunya.
+nama_mahasiswa_jurusan_informatika = "Arya"
+print("nama_mahasiswa_jurusan_informatika",nama_mahasiswa_jurusan_informatika)
+
+#Aturan 3: Penamaan variabel bersifat case sensitive artinya bahwa kita harus konsisten dalam penulisan
+#besar kecil huruf dalam nama variabel
+Lebar = 150
+panjang = 200
+
+print ("panjang", panjang)
+print ("Lebar", Lebar)
+
+#Aturan 4: jangan menggunakan keyword phyton seperti yang sudah dibahas sebelumnya.
+#contoh: and,as,assert,break,class,continue,def,elif,else,except,finally,for,from,with dan lain-lain
+continue = "lanjut"
+#hasilnya akan sintax eror karena continue sudah dikenali oleh phyton.  

ca3f9879a880969ba986b8dfc09688d8d2665b04
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-10-24 10:04:29 +0800

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,27 @@
+
+#Percabangan
+nilai = 90
+if (nilai >= 85):
+    print ("Nilai A")
+
+#Percabangan if else
+nilai = 60
+if (nilai >= 70):
+    print ("LULUS")
+else:
+    print("Tidak LULUS")
+
+#percabangan elif
+umur = 30
+if(umur > 20 and umur < 50):
+    print("Sudah Beranjak Dewasa")
+elif(umur >= 40 and umur <45):
+    print("Masa masa emas")
+elif(umur >= 45 and umur <60):
+    print("Masa masa emas")
+elif(umur >= 60):
+    print("Masa Manula")
+else:
+    print("Masih Dibawah umur")
+
+    

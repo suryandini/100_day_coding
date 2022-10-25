87ffb801177d6399befe63229382cb771cf6ee5b
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-10-25 09:56:55 +0800

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,29 @@
+#Tantangan
+#Aritmatika
+
+def soal():
+    print('''D0217011
+    gaji_pokok =1.000.000
+    gaji_lembur/jam =5.000
+    lama_lembur = sesuai dari angka terakhir nim
+    gaji_lemburnya =(gaji_lembur/jam)lama_lembur
+    pajak = 10%''')
+jumlah_gaji_lembur = 5000*11
+gaji_kena_pajak = 1000000*10/100
+gaji_kotor =1000000 + jumlah_gaji_lembur
+gaji_bersih = gaji_kotor - gaji_kena_pajak
+
+soal()
+print("nama:Sadly Ramli (D0217011)")
+print("gaji pokok : 1000000")
+print("gaji lembur :",jumlah_gaji_lembur)
+print("gaji kotor :",gaji_kotor)
+print("pajak:", 10/100)
+print("potongan :", gaji_kena_pajak)
+print("gaji_bersih :", gaji_bersih)
+
+
+
+    
+
+    

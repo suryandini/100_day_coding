1b98922724f02e743a4b01d036f1ad9640e48ad6
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-11-22 13:31:47 +0800

    Add files via upload

---
+++ day37.py
@@ -0,0 +1,8 @@
+#panjang atau banyak key pada dictionary
+sekolah = {
+    'nama' : 'Smk Negeri 1 Bambang',
+    'jenjang' : 'sekolah menengah atas',
+    'akreditasi' : 'A'
+    }
+print("jumlah atribut variabel sekolah adalah:",len (sekolah))
+

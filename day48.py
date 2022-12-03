e69e67de0db23f2086f963ac09906c575991ada4
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-12-03 20:03:34 +0700

    Add files via upload

---
+++ day48.py
@@ -0,0 +1,10 @@
+for i in range(3):
+    print("perulanagn luar [i] = ", i)
+
+    for j in range(10):
+        print(' perulangan dalam [i, j] =', str(i) +','+str(j))
+
+ulang = 15
+
+for i in range(ulang):
+    print(f"Loop ke-{i}")

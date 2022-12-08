3eb4f069f4853699d57583b1158ccd9a19ee5484
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-12-08 20:01:36 +0700

    Add files via upload

---
+++ day53.py
@@ -0,0 +1,14 @@
+print("program menghitung volume kubus")
+sisi = float(input("Masukan sisi:"))
+hasil = sisi+sisi+sisi
+print("volume kubus adalah:"+ str(hasil))
+
+print("==========================================")
+
+print("Menghitung vome balok")
+p = 6
+l = 4
+t = 5
+volume = p*l*t
+print("Volume kubus adalah=",volume)
+

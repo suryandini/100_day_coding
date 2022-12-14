c80052516c51fa40d09345039fa0e713799fd48e
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-12-14 21:39:05 +0700

    Add files via upload

--- day58.py
+++ day58.py
@@ -1,38 +1,22 @@
-print("=======Menghitung kecepatan dan luas========")
-def hitung_kecepatan():
-    print('hitung kecepatan ready')
-    jarak = float(input("masukan jarak:"))
-    waktu = float(input("masukan waktu:"))
-    kecepatan = jarak * waktu
-    print("kecepatan: ", kecepatan,'\n')
 
-def luas_segitiga():
-    print("hitung luas segitiga :")
-    alas = float(input("Masukan alas: "))
-    tinggi = float(input("Masukan tinggi: "))
-    luas = 0.5 *(alas * tinggi)
-    print("luas segitiga adalah:", luas, "cm", "\n")
+def kerucut(rk):
+ return 1/3*22/7*rk**2*t
+def bola (r):
+ return 4/3*22/7*r**3
+if __name__ == "__main__":
 
-def luas_balok():
-     print("hitung luas balok:")
-     panjang = float(input("Masukan panjang: "))
-     lebar = float(input("Masukan lebar:"))
-     tinggi = float(input("Masukan tinggi:"))
-     luas = (2*panjang*lebar) + (2*panjang*tinggi) + (2*lebar*tinggi)
-     print("luas balok adalah: ", luas, "cm", "\n")
+ print("Menghitung Volume Kerucut dalam Bola \n")
 
-    
-while True:
-    userInput = int(input(
-        "Pilih rumus yang akan di pakai: \n\n1.hitung kecepatan\n2.luas segitiga\n3.luas_balok\n\n0.keluar program\n\npilih nomor berapa: "))
-        
-    if(userInput == 1):
-         hitung_kecepatan()
-    elif(userInput == 2):
-         luas_segitiga()
-    elif(userInput == 3):
-         luas_balok()
-    
-    else:
-        break
+r = int(input("Masukkan jari-jari bola: "))
+rk = int(input("Masukkan jari-jari alas kerucut: "))
+t = int(input("Masukkan tinggi kerucut: "))
 
+# variable
+a = kerucut(rk)
+b = bola (r)
+
+
+print("\n")
+
+print("Volume kerucut adalah = {}".format(a))
+print("Volume bola adalah = {}".format(b))

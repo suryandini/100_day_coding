ea72841fa9502fbf2a3191b02d813c7727989d8d
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-12-22 14:37:45 +0800

    Add files via upload

---
+++ day67.py
@@ -0,0 +1,25 @@
+#Kelas manusia sebagai "defenisi"
+class Manusia:
+    nama = None
+    usia = None
+    Alamat = None
+
+#membangun instance/variabel sebagai "objek nyata"
+manusia1 = Manusia()
+manusia1.nama = "Suryandini"
+manusia1.usia = "18 Tahun"
+manusia1.Alamat = "Alamat_Mambi"
+
+manusia2 = Manusia()
+manusia2.nama = "Musdalifa"
+manusia2.usia = "20 Tahun"
+manusia2.Alamat = "Alamat_Polewali"
+
+manusia3 = Manusia()
+manusia3.nama = "Nurlaelia"
+manusia3.usia = "19 Tahun"
+manusia3.Alamat = "Alamat_campalagian"
+
+print(manusia1.nama, manusia1.usia, manusia1.Alamat)
+print(manusia2.nama, manusia2.usia, manusia2.Alamat)
+print(manusia3.nama, manusia3.usia, manusia3.Alamat)

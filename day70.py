0a0986c7ac3f83efc5d60108d1d32842bca7f62d
Suryandini <100518448+suryandini@users.noreply.github.com>
2022-12-25 08:23:05 +0800

    Add files via upload

---
+++ day70.py
@@ -0,0 +1,24 @@
+a = 20/5
+print(type(a))
+
+b= 12/2
+print(type(b))
+
+print('*' * 20)
+
+c = 25 + 6
+print(type(c))
+
+d = 12 + 3.0
+print(type(d))
+
+e = 10.6 - 6
+print(type(e))
+
+print('*' * 15)
+
+kata = 'apel' * 6
+print(kata)
+print(type(kata))
+
+print('*' * 12)

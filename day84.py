3c90061e82fbf891e35f4b7c940452234fc72478
Suryandini <100518448+suryandini@users.noreply.github.com>
2023-01-08 20:16:59 +0800

    Add files via upload

---
+++ day84.py
@@ -0,0 +1,9 @@
+#menghitung total dari pembelian buku 
+buku = "Phyton for Beginners" 
+harga = 35000
+jumlah_beli = 7 
+
+total_harga = harga * jumlah_beli
+
+print("anda telah membeli buku '" + buku +"' sebanyak" + str(jumlah_beli) + "buah") 
+print("total harga yang harus di bayar : Rp. " + str(total_harga)) 

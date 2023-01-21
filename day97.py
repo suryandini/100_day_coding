cb7998cd48b7ebb815c76e181d5375ec02b30336
Suryandini <100518448+suryandini@users.noreply.github.com>
2023-01-21 21:01:09 +0800

    Add files via upload

---
+++ day97.py
@@ -0,0 +1,14 @@
+gaji_pokok = 5000000
+uang_kost = 750000
+uang_tabung = 1500000
+harga_komputer = 55000000
+
+# Hitung sisa uang yang dapat ditabung setiap bulannya
+sisa_uang = gaji_pokok - uang_kost - uang_tabung
+
+# Hitung berapa bulan yang dibutuhkan untuk menabung
+bulan = harga_komputer / sisa_uang
+
+# Tampilkan hasil
+print("DINI harus menabung selama minimal", bulan, "bulan untuk dapat membeli Komputer Sultan.")
+
